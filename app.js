247683949d9b5e2c888f4d61eeabf9ac2a5b0ac3
Apiipakar <cayuuni37@gmail.com>
2022-12-02 11:19:43 -0800

    first_commit

--- app.js
+++ app.js
@@ -12,28 +12,28 @@ const subscribeBTn = document.querySelector("#subscribeBTn");
 const people = [
   {
     id: 1,
-    image: "../third project/images/apiipakar_one.jpg",
+    image: "./images/apiipakar_one.jpg",
     name: "Apiipakar",
     job: "designer",
     text: "i would like to work for you because i think you welling someone todesign your brand, as all we know every success project is willing tohave great and awesome discriminated design.",
   },
   {
     id: 2,
-    image: "../third project/images/apipakar2.jpg",
+    image: "./images/apipakar2.jpg",
     name: "abuubakar",
     job: "sofware Engineer",
     text: "i would like to work for your software as ican implement, analys and develop your software in needs, so i am available any time to join your army.",
   },
   {
     id: 3,
-    image: "../third project/images/apiipakar3.jpg",
+    image: "./images/apiipakar3.jpg",
     name: "apuukar",
     job: "web developer",
     text: "if you need quality and more effeciencly website i am here to work for you, as my skills is to develop and turn any dream into working dynamic website.",
   },
   {
     id: 4,
-    image: "../third project/images/person_one.jpg",
+    image: "./images/person_one.jpg",
     name: "person_one",
     job: "Entartainer",
     text: "Just subme if you need good entertainer to make your day you and your childs so i am here for server.",
